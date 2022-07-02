6da88b0698fe17b48035c4d679c559fd32ea0ec0
Ivan Meleshko <ivanmeleshko@gmail.com>
2022-07-02 22:47:52 +0300

    Initial commit

---
+++ StudentList/src/com/skillfactory/studentlist/Main.java
@@ -0,0 +1,50 @@
+package com.skillfactory.studentlist;
+
+import java.util.Scanner;
+
+import com.skillfactory.studentlist.StudentStorage.SizeException;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		StudentStorage newStorage = new StudentStorage();
+		
+		Student pupkin = new Student("Pupkin", "+893335544", "pupkin@mail.ru");
+		
+		//newStorage.addStudent("Vasiliy Vlasov vasily@mail.ru +8955666433");
+		//newStorage.listStudent();
+		
+		while (true) {
+            String command = sc.nextLine();
+            String[] tokens = command.split("\\s+", 2);
+            if (tokens[0].equals("add")) {
+                newStorage.addStudent(tokens[1]);		
+            } else if (tokens[0].equals("list")) {
+                newStorage.listStudent();
+            } else if (tokens[0].equals("get")) {
+                newStorage.getStudentByName(tokens[1]).toString();
+            } else if (tokens[0].equals("remove")) {
+                newStorage.removeStudent(tokens[1]);
+            } else if (tokens[0].equals("count")) {
+                System.out.println("There are " + newStorage.getCount() + " customers");
+            } else if (tokens[0].equals("help")) {
+                System.out.println(helpText);
+            } else {
+                System.out.println(commandError);
+            }
+        }
+
+	}
+	
+	 private static String addCommand = "add Василий Петров " +
+	            "vasily.petrov@gmail.com +79787775747";
+	    private static String commandExamples = "\t" + addCommand + "\n" +
+	            "\tlist\n\tcount\n\tremove Василий Петров";
+	    private static String commandError = "Wrong command! Available command examples: \n" +
+	            commandExamples;
+	    private static String helpText = "Command examples:\n" + commandExamples;
+
+
+}
---
+++ StudentList/src/com/skillfactory/studentlist/StudentStorage.java
@@ -0,0 +1,79 @@
+package com.skillfactory.studentlist;
+
+import java.util.HashMap;
+
+public class StudentStorage {
+	private HashMap<String, Student> storage;
+
+	public StudentStorage() {
+		storage = new HashMap<>();
+	}
+
+//	public void addStudent(String data) throws SizeException {
+//		String[] components = data.split("\\s+");
+//		if (components.length != 4) { throw new SizeException("Wrong word quant");}
+//		String name = components[0] + " " + components[1];
+//		storage.put(name, new Student(name, components[3], components[2]));
+//		
+//	}
+
+	public void addStudent(String data) {
+		try {
+			String[] components = data.split("\\s+");
+			if (components.length != 4) {
+				throw new SizeException("Wrong words quant");
+			}
+			String name = components[0] + " " + components[1];
+			storage.put(name, new Student(name, components[3], components[2]));
+		} catch (SizeException se) {
+			System.out.println(se.message);
+		}
+
+	}
+
+	public void listStudent() {
+		try {
+			if (storage.size() == 0) {
+				throw new SizeException("The storage is empty");
+			}
+			storage.values().forEach(System.out::println);
+		} catch (SizeException e) {
+			System.out.println(e.message);
+		}
+	}
+
+	public void removeStudent(String name) {
+		try {
+			if (!storage.containsKey(name)) { 
+				throw new MissException("No such student");
+			}
+			storage.remove(name);
+		} catch (MissException e) {
+			System.out.println(e.message);
+		}
+	}
+
+	public Student getStudentByName(String name) {	
+			return storage.get(name); 
+	}
+
+	public int getCount() {
+		return storage.size();
+	}
+
+	public class SizeException extends Exception {
+		String message;
+
+		public SizeException(String message) {
+			this.message = message;
+		}
+	}
+	
+	public class MissException extends Exception {
+		String message;
+
+		public MissException(String message) {
+			this.message = message;
+		}
+	}
+}
